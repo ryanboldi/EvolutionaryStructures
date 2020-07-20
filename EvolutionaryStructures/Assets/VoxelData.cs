eae7a73a6563a7506eeb9b2fe05e984c4c0d5aca
Ryan Boldi <Ryan.boldi123@gmail.com>
2020-07-20 13:33:37 +0400

    Update VoxelData.cs

--- EvolutionaryStructures/Assets/VoxelData.cs
+++ EvolutionaryStructures/Assets/VoxelData.cs
@@ -10,16 +10,50 @@ public class VoxelData {
     }
 
     public int Depth {
-        get { return data.GetLength(1); }
+        get { return data.GetLength(2); }
     }
 
     public int Height {
-        get { return data.GetLength(2); }
+        get { return data.GetLength(1); }
     }
 
     public int GetCell(int x, int y, int z) {
         return data[x, y, z];
     }
+
+    public int GetNeighbour(int x, int y, int z, Direction dir) {
+        DataCoordinate offsetToCheck = offsets[(int)dir];
+        DataCoordinate neighborCoord = new DataCoordinate(x + offsetToCheck.x, y + offsetToCheck.y, z + offsetToCheck.z);
+
+        if (neighborCoord.x < 0 || neighborCoord.x >= Width || neighborCoord.y < 0 || neighborCoord.y >= Height || neighborCoord.z < 0 || neighborCoord.z >= Depth) {
+            //we know for sure it's outside data, it is empty
+            return 0;
+        } else {
+            return GetCell(neighborCoord.x, neighborCoord.y, neighborCoord.z);
+        }
+    }
+
+    struct DataCoordinate {
+        public int x;
+        public int y;
+        public int z;
+
+        public DataCoordinate(int x, int y, int z) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    }
+
+    DataCoordinate[] offsets = {
+        new DataCoordinate(0,0,1),
+        new DataCoordinate(1,0,0),
+        new DataCoordinate(0,0,-1),
+        new DataCoordinate(-1,0,0),
+        new DataCoordinate(0,1,0),
+        new DataCoordinate(0,-1,0)
+    };
+
 }
 
 
